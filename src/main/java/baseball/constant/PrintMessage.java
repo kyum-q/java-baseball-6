86c60af342cfe8723633cf39f14539e11325806e
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-12 23:35:25 +0900

    feat 출력문 구현

--- src/main/java/baseball/constant/PrintMessage.java
+++ src/main/java/baseball/constant/PrintMessage.java
@@ -2,22 +2,16 @@ package baseball.constant;
 
 public enum PrintMessage {
     PRINT_GAME_START("숫자 야구 게임을 시작합니다."),
+    PRINT_NOTHING("낫싱"),
+    PRINT_BALL("볼 "),
+    PRINT_STRIKE("스트라이크"),
 
-    PRINT_GAME_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
-    PRINT_NOTHING("낫싱");
+    PRINT_GAME_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
     private String message;
 
     PrintMessage(String s) {
         message = s;
-    }
-
-    PrintMessage(int ball, int strike) {
-        message = "";
-        if(ball != 0)
-            message += ball + "볼 ";
-        if(strike != 0)
-            message += strike + "스트라이크";
     }
 
     public String getValue() {
