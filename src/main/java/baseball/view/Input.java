1ee5ced749d56d7621f43815c8998c0cea46b05a
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-12 23:34:19 +0900

    feat 입력 구현

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -3,15 +3,19 @@ package baseball.view;
 import baseball.constant.ErrorMessage;
 import baseball.constant.RequestMessage;
 import baseball.validator.NumbersValidator;
+import baseball.validator.ReStartValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
 public class Input {
     private NumbersValidator numValidator;
+    private ReStartValidator reStartValidator;
     public Input() {
         numValidator = new NumbersValidator();
+        reStartValidator = new ReStartValidator();
     }
+
     public List<Integer> inputUserNumbers() {
         String printMessage = RequestMessage.REQUEST_THREE_NUMBER.getValue();
         String input;
@@ -23,5 +27,18 @@ public class Input {
         } while(!numValidator.valid(input));
 
         return numValidator.getLists();
+    }
+
+    public int inputUserEnding() {
+        String printMessage = RequestMessage.REQUEST_RESTART.getValue();
+        String input;
+        do {
+            System.out.println(printMessage);
+
+            input = Console.readLine();
+            printMessage = ErrorMessage.ERROR_START_NUM.getMessage();
+        } while(!reStartValidator.valid(input));
+
+        return reStartValidator.getNumber();
     }
 }
