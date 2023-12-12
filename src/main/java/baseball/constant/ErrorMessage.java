a4e62d9e9629bcd397def853fbfb1008d4e3239e
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-12 23:35:34 +0900

    feat 오류 메시지 구현

--- src/main/java/baseball/constant/ErrorMessage.java
+++ src/main/java/baseball/constant/ErrorMessage.java
@@ -1,7 +1,7 @@
 package baseball.constant;
 
 public enum ErrorMessage {
-    ERROR_THREE_NUM("1부터 9까지 서로 다른 수 3개를 입력해주세요."),
+    ERROR_THREE_NUM("1부터 9까지 서로 다른 수 3개를 입력해주세요. : "),
     ERROR_START_NUM("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
     private String message;
@@ -11,6 +11,6 @@ public enum ErrorMessage {
     }
 
     public String getMessage() {
-        return "[ERROR] " + message;
+        return message;
     }
 }
